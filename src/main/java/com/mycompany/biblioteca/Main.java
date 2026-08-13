0b651ac1297d112a1ab4edf7e5a9857ddba90828
Merlin Landero <mlandero@unicesar.edu.co>
2026-08-12 22:04:12 -0500

    feat: Inicializar proyecto Maven de Biblioteca

---
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.biblioteca;
+
+/**
+ *
+ * @author 57300
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
